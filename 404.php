206a7c5cf6622ff6ade5927a353182b3c643652a
Mustafo Alisherovich <mustafoalisherovich75@gmail.com>
2023-07-08 23:17:43 +0500

    first commit

---
+++ 404.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Amin Template">
+    <meta name="keywords" content="Amin, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Amin | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel:400,700,900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Humberger Menu Begin -->
+    <div class="humberger-menu-overlay"></div>
+    <div class="humberger-menu-wrapper">
+        <div class="hw-logo">
+            <a href="#"><img src="img/f-logo.png" alt=""></a>
+        </div>
+        <div class="hw-menu mobile-menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Platform <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Playstation</a></li>
+                        <li><a href="#">Windows</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Xbox</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Reviews <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Playstation</a></li>
+                        <li><a href="#">Windows</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Xbox</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Windows <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Playstation</a></li>
+                        <li><a href="#">Windows</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Xbox</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Videos <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Playstation</a></li>
+                        <li><a href="#">Windows</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Xbox</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div id="mobile-menu-wrap"></div>
+        <div class="hw-copyright">
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        </div>
+        <div class="hw-social">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-youtube-play"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
+        <div class="hw-insta-media">
+            <div class="section-title">
+                <h5>Instagram</h5>
+            </div>
+            <div class="insta-pic">
+                <img src="img/instagram/ip-1.jpg" alt="">
+                <img src="img/instagram/ip-2.jpg" alt="">
+                <img src="img/instagram/ip-3.jpg" alt="">
+                <img src="img/instagram/ip-4.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <!-- Humberger Menu End -->
+
+    <!-- Header Section Begin -->
+   <?php
+   include_once('./include/header.php');
+   ?>
+    <!-- Header End -->
+
+    <!-- 404 Section Begin -->
+    <section class="section-404">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-404">
+                        <h1>404</h1>
+                        <h3>Opps! This page Could Not Be Found!</h3>
+                        <p>Sorry bit the page you are looking for does not exist, have been removed or name changed</p>
+                        <form action="#" class="search-404">
+                            <input type="text" placeholder="Enter your keyword">
+                            <button type="submit"><i class="fa fa-search"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- 404 Section End -->
+
+    <!-- Latest Preview Section Begin -->
+    <section class="latest-preview-section page-404">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h5>Latest Preview</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="lp-slider owl-carousel">
+                    <div class="col-lg-3">
+                        <div class="lp-item">
+                            <div class="lp-pic set-bg" data-setbg="img/latest-preview/lp-1.jpg">
+                                <div class="review-loader">
+                                    <div class="loader-circle-wrap">
+                                        <div class="loader-circle">
+                                            <span class="circle-progress" data-cpid="id" data-cpvalue="75"
+                                                data-cpcolor="#c20000"></span>
+                                            <div class="review-point">7.5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lp-text">
+                                <h6><a href="#">Teamfight Tactics is in chaos after patch...</a></h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="lp-item">
+                            <div class="lp-pic set-bg" data-setbg="img/latest-preview/lp-2.jpg">
+                                <div class="review-loader">
+                                    <div class="loader-circle-wrap">
+                                        <div class="loader-circle">
+                                            <span class="circle-progress" data-cpid="id" data-cpvalue="75"
+                                                data-cpcolor="#c20000"></span>
+                                            <div class="review-point">7.5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lp-text">
+                                <h6><a href="#">Lenovo’s y540 gaming laptop with an rtx 2060...</a></h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="lp-item">
+                            <div class="lp-pic set-bg" data-setbg="img/latest-preview/lp-3.jpg">
+                                <div class="review-loader">
+                                    <div class="loader-circle-wrap">
+                                        <div class="loader-circle">
+                                            <span class="circle-progress" data-cpid="id" data-cpvalue="75"
+                                                data-cpcolor="#c20000"></span>
+                                            <div class="review-point">7.5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lp-text">
+                                <h6><a href="#">A monter prom poster got hijacked for a...</a></h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="lp-item">
+                            <div class="lp-pic set-bg" data-setbg="img/latest-preview/lp-4.jpg">
+                                <div class="review-loader">
+                                    <div class="loader-circle-wrap">
+                                        <div class="loader-circle">
+                                            <span class="circle-progress" data-cpid="id" data-cpvalue="75"
+                                                data-cpcolor="#c20000"></span>
+                                            <div class="review-point">7.5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lp-text">
+                                <h6><a href="#">Facebook wants to read your thoughts with...</a></h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="lp-item">
+                            <div class="lp-pic set-bg" data-setbg="img/latest-preview/lp-4.jpg">
+                                <div class="review-loader">
+                                    <div class="loader-circle-wrap">
+                                        <div class="loader-circle">
+                                            <span class="circle-progress" data-cpid="id" data-cpvalue="75"
+                                                data-cpcolor="#c20000"></span>
+                                            <div class="review-point">7.5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lp-text">
+                                <h6><a href="#">Facebook wants to read your thoughts with...</a></h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="lp-item">
+                            <div class="lp-pic set-bg" data-setbg="img/latest-preview/lp-4.jpg">
+                                <div class="review-loader">
+                                    <div class="loader-circle-wrap">
+                                        <div class="loader-circle">
+                                            <span class="circle-progress" data-cpid="id" data-cpvalue="75"
+                                                data-cpcolor="#c20000"></span>
+                                            <div class="review-point">7.5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lp-text">
+                                <h6><a href="#">Facebook wants to read your thoughts with...</a></h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="lp-item">
+                            <div class="lp-pic set-bg" data-setbg="img/latest-preview/lp-4.jpg">
+                                <div class="review-loader">
+                                    <div class="loader-circle-wrap">
+                                        <div class="loader-circle">
+                                            <span class="circle-progress" data-cpid="id" data-cpvalue="75"
+                                                data-cpcolor="#c20000"></span>
+                                            <div class="review-point">7.5</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lp-text">
+                                <h6><a href="#">Facebook wants to read your thoughts with...</a></h6>
+                                <ul>
+                                    <li><i class="fa fa-clock-o"></i> Aug 01, 2019</li>
+                                    <li><i class="fa fa-comment-o"></i> 12</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Latest Preview Section End -->
+
+    <!-- Footer Section Begin -->
+   <?php
+   include_once('./include/footer.php');
+   ?>
+    <!-- Footer Section End -->
+
+    <!-- Sign Up Section Begin -->
+    <div class="signup-section">
+        <div class="signup-close"><i class="fa fa-close"></i></div>
+        <div class="signup-text">
+            <div class="container">
+                <div class="signup-title">
+                    <h2>Sign up</h2>
+                    <p>Fill out the form below to recieve a free and confidential</p>
+                </div>
+                <form action="#" class="signup-form">
+                    <div class="sf-input-list">
+                        <input type="text" class="input-value" placeholder="User Name*">
+                        <input type="text" class="input-value" placeholder="Password">
+                        <input type="text" class="input-value" placeholder="Confirm Password">
+                        <input type="text" class="input-value" placeholder="Email Address">
+                        <input type="text" class="input-value" placeholder="Full Name">
+                    </div>
+                    <div class="radio-check">
+                        <label for="rc-agree">I agree with the term & conditions
+                            <input type="checkbox" id="rc-agree">
+                            <span class="checkbox"></span>
+                        </label>
+                    </div>
+                    <button type="submit"><span>REGISTER NOW</span></button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Sign Up Section End -->
+
+    <!-- Search model Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search model end -->
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/circle-progress.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Amin Template">
+    <meta name="keywords" content="Amin, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Amin | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cinzel:400,700,900&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Humberger Menu Begin -->
+    <div class="humberger-menu-overlay"></div>
+    <div class="humberger-menu-wrapper">
+        <div class="hw-logo">
+            <a href="#"><img src="img/f-logo.png" alt=""></a>
+        </div>
+        <div class="hw-menu mobile-menu">
+            <ul>
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="#">Pages <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="./categories-list.html">Categories</a></li>
+                        <li><a href="./categories-grid.html">Categories grid</a></li>
+                        <li><a href="./typography.html">Typography</a></li>
+                        <li><a href="./details-post-default.html">Post default</a></li>
+                        <li><a href="./details-post-gallery.html">Post gallery</a></li>
+                        <li><a href="./details-post-review.html">Post review</a></li>
+                        <li><a href="./contact.html">Contact</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Reviews <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Playstation</a></li>
+                        <li><a href="#">Windows</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Xbox</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Windows <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Playstation</a></li>
+                        <li><a href="#">Windows</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Xbox</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Videos <i class="fa fa-angle-down"></i></a>
+                    <ul class="dropdown">
+                        <li><a href="#">Playstation</a></li>
+                        <li><a href="#">Windows</a></li>
+                        <li><a href="#">Mobile</a></li>
+                        <li><a href="#">Xbox</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div id="mobile-menu-wrap"></div>
+        <div class="hw-copyright">
+            Copyright © 2019 Colorlib Inc. All rights reserved
+        </div>
+        <div class="hw-social">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-youtube-play"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
+        <div class="hw-insta-media">
+            <div class="section-title">
+                <h5>Instagram</h5>
+            </div>
+            <div class="insta-pic">
+                <img src="img/instagram/ip-1.jpg" alt="">
+                <img src="img/instagram/ip-2.jpg" alt="">
+                <img src="img/instagram/ip-3.jpg" alt="">
+                <img src="img/instagram/ip-4.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <!-- Humberger Menu End -->
+
+    <!-- Header Section Begin -->
+   <?php
+   include_once('/include/header.php');
+   ?>
+    <!-- Header End -->
+
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg spad" data-setbg="img/breadcrumb-bg.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb-text">
+                        <h3>Contact us</h3>
+                        <div class="bt-option">
+                            <a href="./home.html">Home</a>
+                            <span>Contact</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
+    <!-- Contact Section Begin -->
+    <section class="contact-section spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="contact-text">
+                        <div class="contact-title">
+                            <h3>Contact us</h3>
+                            <p>My experience with Realy is absolutely positive. The themes are beautifully<br />
+                                designed and well documented. Realy theme provides quick support.</p>
+                        </div>
+                        <div class="contact-form">
+                            <div class="dt-leave-comment">
+                                <form action="#">
+                                    <div class="input-list">
+                                        <input type="text" placeholder="Name">
+                                        <input type="text" placeholder="Email">
+                                        <input type="text" placeholder="Website">
+                                    </div>
+                                    <textarea placeholder="Message"></textarea>
+                                    <button type="submit">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
+
+    <!-- Footer Section Begin -->
+    <?php
+    include_once('./include/footer.php');
+    ?>
+    <!-- Footer Section End -->
+
+    <!-- Sign Up Section Begin -->
+    <div class="signup-section">
+        <div class="signup-close"><i class="fa fa-close"></i></div>
+        <div class="signup-text">
+            <div class="container">
+                <div class="signup-title">
+                    <h2>Sign up</h2>
+                    <p>Fill out the form below to recieve a free and confidential</p>
+                </div>
+                <form action="#" class="signup-form">
+                    <div class="sf-input-list">
+                        <input type="text" class="input-value" placeholder="User Name*">
+                        <input type="text" class="input-value" placeholder="Password">
+                        <input type="text" class="input-value" placeholder="Confirm Password">
+                        <input type="text" class="input-value" placeholder="Email Address">
+                        <input type="text" class="input-value" placeholder="Full Name">
+                    </div>
+                    <div class="radio-check">
+                        <label for="rc-agree">I agree with the term & conditions
+                            <input type="checkbox" id="rc-agree">
+                            <span class="checkbox"></span>
+                        </label>
+                    </div>
+                    <button type="submit"><span>REGISTER NOW</span></button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Sign Up Section End -->
+
+    <!-- Search model Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search model end -->
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/circle-progress.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
